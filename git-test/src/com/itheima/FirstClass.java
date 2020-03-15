82084302c9c847f9dcd8e04a125b633dc6d46140
wangzhiwen <zhansanitcast@.com>
2020-03-16 00:06:39 +0800

    吧本地工成天价到git

---
+++ git-test/src/com/itheima/FirstClass.java
@@ -0,0 +1,15 @@
+package com.itheima;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("nihao git");
+        System.out.println("nihao git");
+        System.out.println("nihao git");
+        System.out.println("nihao git");
+        System.out.println("nihao git");
+        System.out.println("nihao git");
+        System.out.println("nihao git");
+
+
+    }
+}
